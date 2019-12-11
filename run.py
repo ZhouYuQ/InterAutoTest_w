752bd14a9b09c5a5477ef10e48fbc7516db753e2
ZhouYuQ <18500096975@163.com>
2019-12-11 15:44:29 +0800

    更新run.py

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
+import os
 
+import pytest
+
+from config import Conf
+
+if __name__ == '__main__':
+    report_path = Conf.get_report_path()+os.sep+"result"
+    report_html_path = Conf.get_report_path()+os.sep+"html"
+    pytest.main(["s","--alluredir",report_path])

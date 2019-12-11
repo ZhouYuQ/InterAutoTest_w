99c6d63d7475ac4978ed5b00f1c801dd2bbb499d
ZhouYuQ <57940858+ZhouYuQ@users.noreply.github.com>
2019-12-12 00:08:33 +0800

    Update run.py

--- run.py
+++ run.py
@@ -7,5 +7,5 @@ from config import Conf
 if __name__ == '__main__':
     report_path = Conf.get_report_path()+os.sep+"result"
     report_html_path = Conf.get_report_path()+os.sep+"html"
-    pytest.main(["test_excel_case.py", "--alluredir","./report/result"])
+    pytest.main(["./testcase/test_excel_case.py", "--alluredir",report_path])
     # pytest.main(["test_excel_case.py", "--alluredir",report_path])
